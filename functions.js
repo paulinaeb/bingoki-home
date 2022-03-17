158f7cf2bb15a974c51325a99295cb044ec872a2
paulinaeb <pdespejo18@gmail.com>
2022-03-16 22:06:30 -0400

    box de premios con carousel done

--- functions.js
+++ functions.js
@@ -28,3 +28,34 @@ var slideIndex = 1;
         slides[slideIndex - 1].style.display = "block";
         dots[slideIndex - 1].className += " active";
       }
+
+      var slideIndex2 = 1;
+      showslides2(slideIndex2);
+
+      function plusSlides2(n) {
+        showslides2((slideIndex2 += n));
+      }
+
+      function currentSlide2(n) {
+        showslides2((slideIndex2 = n));
+      }
+
+      function showslides2(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides2");
+        var dots = document.getElementsByClassName("dot2");
+        if (n > slides.length) {
+          slideIndex2 = 1;
+        }
+        if (n < 1) {
+          slideIndex2 = slides.length;
+        }
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex2 - 1].style.display = "block";
+        dots[slideIndex2 - 1].className += " active";
+      }
