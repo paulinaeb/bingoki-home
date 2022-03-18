8724a61aa4faa592bac566c3529ec7736370323a
paulinaeb <pdespejo18@gmail.com>
2022-03-17 20:17:19 -0400

    avances en section 3

--- functions.js
+++ functions.js
@@ -91,3 +91,34 @@ var slideIndex = 1;
         // Refresh every second
         setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
     });
+
+    var slideIndex3 = 1;
+      showslides3(slideIndex3);
+
+      function plusSlides3(n) {
+        showslides3((slideIndex3 += n));
+      }
+
+      function currentSlide3(n) {
+        showslides3((slideIndex3 = n));
+      }
+
+      function showslides3(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides3");
+        var dots = document.getElementsByClassName("dot3");
+        if (n > slides.length) {
+          slideIndex3 = 1;
+        }
+        if (n < 1) {
+          slideIndex3 = slides.length;
+        }
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex3 - 1].style.display = "block";
+        dots[slideIndex3 - 1].className += " active";
+      }
