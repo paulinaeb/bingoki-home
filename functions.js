af977e6c72d899cf9d101de7871e2a82f223ad87
paulinaeb <pdespejo18@gmail.com>
2022-03-19 14:23:31 -0400

    recortar imagenes y section del info de juego maquetada

--- functions.js
+++ functions.js
@@ -1,124 +1,152 @@
 var slideIndex = 1;
-      showSlides(slideIndex);
+showSlides(slideIndex);
 
-      function plusSlides(n) {
-        showSlides((slideIndex += n));
-      }
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-      function currentSlide(n) {
-        showSlides((slideIndex = n));
-      }
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
-      function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {
-          slideIndex = 1;
-        }
-        if (n < 1) {
-          slideIndex = slides.length;
-        }
-        for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-      }
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
 
-      var slideIndex2 = 1;
-      showslides2(slideIndex2);
+var slideIndex2 = 1;
+showslides2(slideIndex2);
 
-      function plusSlides2(n) {
-        showslides2((slideIndex2 += n));
-      }
+function plusSlides2(n) {
+  showslides2((slideIndex2 += n));
+}
 
-      function currentSlide2(n) {
-        showslides2((slideIndex2 = n));
-      }
+function currentSlide2(n) {
+  showslides2((slideIndex2 = n));
+}
 
-      function showslides2(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides2");
-        var dots = document.getElementsByClassName("dot2");
-        if (n > slides.length) {
-          slideIndex2 = 1;
-        }
-        if (n < 1) {
-          slideIndex2 = slides.length;
-        }
-        for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex2 - 1].style.display = "block";
-        dots[slideIndex2 - 1].className += " active";
-      }
+function showslides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {
+    slideIndex2 = 1;
+  }
+  if (n < 1) {
+    slideIndex2 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2 - 1].style.display = "block";
+  dots[slideIndex2 - 1].className += " active";
+}
 
-      // contador regresivo
+// contador regresivo
 
-      document.addEventListener('DOMContentLoaded', () => { 
-        const DATE_TARGET = new Date('05/13/2022 0:01 AM');
-        // DOM for render
-        const SPAN_DAYS = document.querySelector('span#days');
-        const SPAN_HOURS = document.querySelector('span#hours');
-        const SPAN_MINUTES = document.querySelector('span#minutes');
-        const SPAN_SECONDS = document.querySelector('span#seconds');
-        // Milliseconds for the calculations
-        const MILLISECONDS_OF_A_SECOND = 1000;
-        const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
-        const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
-        const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24;
-        function updateCountdown() {
-            // Calcs
-            const NOW = new Date()
-            const DURATION = DATE_TARGET - NOW;
-            const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
-            const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
-            const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
-            const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
-            SPAN_DAYS.textContent = REMAINING_DAYS;
-            SPAN_HOURS.textContent = REMAINING_HOURS;
-            SPAN_MINUTES.textContent = REMAINING_MINUTES;
-            SPAN_SECONDS.textContent = REMAINING_SECONDS;
-        } 
-        updateCountdown();
-        // Refresh every second
-        setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const DATE_TARGET = new Date("05/13/2022 0:01 AM");
+  // DOM for render
+  const SPAN_DAYS = document.querySelector("span#days");
+  const SPAN_HOURS = document.querySelector("span#hours");
+  const SPAN_MINUTES = document.querySelector("span#minutes");
+  const SPAN_SECONDS = document.querySelector("span#seconds");
+  // Milliseconds for the calculations
+  const MILLISECONDS_OF_A_SECOND = 1000;
+  const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+  const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+  const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24;
+  function updateCountdown() {
+    // Calcs
+    const NOW = new Date();
+    const DURATION = DATE_TARGET - NOW;
+    const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+    const REMAINING_HOURS = Math.floor(
+      (DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR
+    );
+    const REMAINING_MINUTES = Math.floor(
+      (DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE
+    );
+    const REMAINING_SECONDS = Math.floor(
+      (DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND
+    );
+    SPAN_DAYS.textContent = REMAINING_DAYS;
+    SPAN_HOURS.textContent = REMAINING_HOURS;
+    SPAN_MINUTES.textContent = REMAINING_MINUTES;
+    SPAN_SECONDS.textContent = REMAINING_SECONDS;
+  }
+  updateCountdown();
+  // Refresh every second
+  setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
+});
 
-    var slideIndex3 = 1;
-      showslides3(slideIndex3);
+var slideIndex3 = 1;
+showslides3(slideIndex3);
 
-      function plusSlides3(n) {
-        showslides3((slideIndex3 += n));
-      }
+function plusSlides3(n) {
+  showslides3((slideIndex3 += n));
+}
 
-      function currentSlide3(n) {
-        showslides3((slideIndex3 = n));
-      }
+function currentSlide3(n) {
+  showslides3((slideIndex3 = n));
+}
 
-      function showslides3(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides3");
-        var dots = document.getElementsByClassName("dot3");
-        if (n > slides.length) {
-          slideIndex3 = 1;
-        }
-        if (n < 1) {
-          slideIndex3 = slides.length;
-        }
-        for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex3 - 1].style.display = "block";
-        dots[slideIndex3 - 1].className += " active";
-      }
+function showslides3(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides3");
+  var dots = document.getElementsByClassName("dot3");
+  if (n > slides.length) {
+    slideIndex3 = 1;
+  }
+  if (n < 1) {
+    slideIndex3 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex3 - 1].style.display = "block";
+  dots[slideIndex3 - 1].className += " active";
+}
+
+//cartas de bingo carousel
+const buttonsWrapper = document.querySelector(".map");
+const slides = document.querySelector(".inner");
+
+buttonsWrapper.addEventListener("click", (e) => {
+  if (e.target.nodeName === "BUTTON") {
+    Array.from(buttonsWrapper.children).forEach((item) =>
+      item.classList.remove("active")
+    );
+    if (e.target.classList.contains("first")) {
+      slides.style.transform = "translateX(-0%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("second")) {
+      slides.style.transform = "translateX(-33.33333333333333%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("third")) {
+      slides.style.transform = "translatex(-66.6666666667%)";
+      e.target.classList.add("active");
+    }
+  }
+});
