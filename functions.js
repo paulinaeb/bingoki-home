cf442ef70e82584cbc2889e899d86d183dbc7891
paulinaeb <pdespejo18@gmail.com>
2022-03-20 19:52:55 -0400

    seccion ganadores + responsive, imagenes del footer

--- functions.js
+++ functions.js
@@ -150,3 +150,35 @@ buttonsWrapper.addEventListener("click", (e) => {
     }
   }
 });
+// slider premios ganados
+var slideIndex4 = 1;
+showslides4(slideIndex4);
+
+function plusSlides4(n) {
+  showslides4((slideIndex4 += n));
+}
+
+function currentSlide4(n) {
+  showslides4((slideIndex4 = n));
+}
+
+function showslides4(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides4");
+  var dots = document.getElementsByClassName("dot4");
+  if (n > slides.length) {
+    slideIndex4 = 1;
+  }
+  if (n < 1) {
+    slideIndex4 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex4 - 1].style.display = "block";
+  dots[slideIndex4 - 1].className += " active";
+}
+
